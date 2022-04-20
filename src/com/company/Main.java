39e417e082979ae0e0d96ac3b3b6aa6bfcd6c33f
Lekesky <61468937+Lekesky@users.noreply.github.com>
2022-04-20 14:32:20 -0400

    exercise 13

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,90 @@
 package com.company;
 
+import java.util.concurrent.Semaphore;
+
 public class Main {
-
+    static int people = 20;
+    static Semaphore sem = new Semaphore(2, true);
     public static void main(String[] args) {
-	    Hi obj1 = new Hi();
-        Hello obj2 = new Hello();
 
-        Thread t1 = new Thread(obj1);
-        Thread t2 = new Thread(obj2);
+
+        Timer time = new Timer();
+	    Restroom1 rr1 = new Restroom1();
+        Restroom2 rr2 = new Restroom2();
+        Restroom3 rr3 = new Restroom3();
+        Restroom4 rr4 = new Restroom4();
+
+
+        Thread t1 = new Thread(time);
+        Thread t2 = new Thread(rr1);
+        Thread t3 = new Thread(rr2);
+        Thread t4 = new Thread(rr3);
+        Thread t5 = new Thread(rr4);
 
         t1.start();
         t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
 
     }
 }
 
-class Hi implements Runnable{
+class Timer implements Runnable{
     public void run(){
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hi");
+        for (int i = 0; i < 30; i++) {
+            System.out.println(i);
             try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
         }
     }
 }
 
-class Hello implements Runnable{
+class Restroom1 implements Runnable{
     public void run(){
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hello");
-            try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+        for (int i = 0; i < Main.people; i++) {
+            try {Main.sem.acquire(1);} catch (InterruptedException e) {e.printStackTrace();}
+            Main.people--;
+            System.out.println("Restroom1 in use");
+            try {Thread.sleep(2250);} catch (InterruptedException e) {e.printStackTrace();}
+            Main.sem.release();
+            System.out.println("Restroom1 Done");
+        }
+    }
+}
+
+class Restroom2 implements Runnable{
+    public void run(){
+        for (int i = 0; i < Main.people; i++) {
+            try {Main.sem.acquire(1);} catch (InterruptedException e) {e.printStackTrace();}
+            Main.people--;
+            System.out.println("Restroom2 in use");
+            try {Thread.sleep(2000);} catch (InterruptedException e) {e.printStackTrace();}
+            Main.sem.release();
+            System.out.println("Restroom2 Done");
+        }
+    }
+}
+class Restroom3 implements Runnable{
+    public void run(){
+        for (int i = 0; i < Main.people; i++) {
+            try {Main.sem.acquire(1);} catch (InterruptedException e) {e.printStackTrace();}
+            Main.people--;
+            System.out.println("Restroom3 in use");
+            try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}
+            Main.sem.release();
+            System.out.println("Restroom3 Done");
+        }
+    }
+}
+class Restroom4 implements Runnable{
+    public void run(){
+        for (int i = 0; i < Main.people; i++) {
+            try {Main.sem.acquire();} catch (InterruptedException e) {e.printStackTrace();}
+            Main.people--;
+            System.out.println("Restroom4 in use");
+            try {Thread.sleep(3500);} catch (InterruptedException e) {e.printStackTrace();}
+            Main.sem.release();
+            System.out.println("Restroom4 Done");
         }
     }
 }
